99d9bb55b904455eb484723d222a26fe14631d78
km <35552602+catnap7777@users.noreply.github.com>
2021-02-15 19:11:41 -0600

    Initial Commit

---
+++ MAD257FirebaseCrashCourse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MAD257FirebaseCrashCourse
+//
+//  Created by Karen Mathes on 2/15/21.
+//  Copyright © 2021 TygerMatrix Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
